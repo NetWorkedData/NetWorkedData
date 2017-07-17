cbf8c2e8e5ab006f77e112d3bd7a8403789be8b0
Jean-François CONTART (iMac) <jfcontart@idemobi.com>
2017-07-17 16:33:25 +0200

    [ADD] progress bar on NWDataManager_CSharp

--- NetWorkedData/NWDEditor/NWDDataManager/NWDDataManager_CSharp.cs
+++ NetWorkedData/NWDEditor/NWDDataManager/NWDDataManager_CSharp.cs
@@ -21,15 +21,24 @@ namespace NetWorkedData
 		#if UNITY_EDITOR
 		public void CreateCShapAllClass () {
 			//Debug.Log ("CreateCShapAllClass");
+			string tProgressBarTitle = "NetWorkedData Create all C# files";
+			float tCountClass = mTypeList.Count + 1;
+			float tOperation = 1;
+			EditorUtility.DisplayProgressBar(tProgressBarTitle, "Create NWDAppConfiguration", tOperation/tCountClass);
+			tOperation++;
 			NWDAppConfiguration.SharedInstance.GenerateCSharpFile (NWDAppConfiguration.SharedInstance.SelectedEnvironment ());
 			foreach( Type tType in mTypeList)
 			{
+				EditorUtility.DisplayProgressBar(tProgressBarTitle, "Create "+tType.Name+" files", tOperation/tCountClass);
+				tOperation++;
 				var tMethodInfo = tType.GetMethod("CreateCSharp", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
 				if (tMethodInfo != null) 
 				{
 					tMethodInfo.Invoke(null, null);
 				}
 			}
+			EditorUtility.DisplayProgressBar(tProgressBarTitle, "Finish", 1.0F);
+			EditorUtility.ClearProgressBar();
 		}
 		#endif
 	}
