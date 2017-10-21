bbb09c3ab0920f26f9046d85705edfab954c8843
kortex <jfcontart@idemobi.com>
2017-10-21 09:28:52 +0200

    [CONVENTIONS]

--- NWDLayoutConvention.cs
+++ NWDLayoutConvention.cs
@@ -13,7 +13,16 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-// Convention of layout for all our unity project!
+// [CONVENTION] Convention of layout for all our unity project in C#!
+// [CONVENTION] Design Pattern
+// [CONVENTION] Class suffix by design pattern CONTART convention 
+// [CONVENTION] xxxxxData
+// [CONVENTION] don't use xxxxxModel
+// [CONVENTION] xxxxxController
+// [CONVENTION] xxxxxView
+// [CONVENTION] xxxxxProvider
+// [CONVENTION] xxxxxService
+// [CONVENTION] xxxxxRequestor
 
 //=====================================================================================================================
 //
@@ -22,29 +31,61 @@
 //
 //=====================================================================================================================
 
-// use delimiter by namespace
-// use delimeter around precompile macro if necessary
+// [CONVENTION] use delimiter by namespace
+// [CONVENTION] use delimeter around precompile macro if necessary
 //=====================================================================================================================
-// namespace all files in projet
+// [CONVENTION] namespace all files in projet
 namespace NetWorkedData
 {
-	// use delimiter by class
-	// use delimeter around precompile macro if necessary
+	// [CONVENTION] use delimiter by class
+	// [CONVENTION] use delimeter around precompile macro if necessary
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	#if UNITY_EDITOR
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	public partial class NWDLayoutClass //In ChamelCase, start by TRIGRAM of project or package
+	// [CONVENTION] HeaderDoc for enum is compulsory indication
+	/// <summary>
+	/// NWD layout enum.
+	/// </summary>
+	public enum NWDLayoutEnum {
+		/// <summary>
+		/// The value a.
+		/// </summary>
+		ValueA, // [CONVENTION] HeaderDoc for each enum value is compulsory indication
+		/// <summary>
+		/// The value b.
+		/// </summary>
+		ValueB,
+		// [CONVENTION] Allways add none value
+
+		None
+	}
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	// [CONVENTION] HeaderDoc for class is compulsory indication
+	public partial class NWDLayoutClass //[CONVENTION] In ChamelCase, start by TRIGRAM of project or package
 	{
-		// use delimiter by region properties group and for all methods
-		// use delimeter around precompile macro if necessary
+		// [CONVENTION] use delimiter by region properties group and for all methods
+		// [CONVENTION] use delimeter around precompile macro if necessary
 		//-------------------------------------------------------------------------------------------------------------
 
 		#region Const
 
 		//-------------------------------------------------------------------------------------------------------------
 		// Insert constants here
-		// Use convention : in uppercase separated by _ start by K_ for public
-		// public const int K_MY_CONSTANT
+		// [CONVENTION] all public before private
+		//-------------------------------------------------------------------------------------------------------------
+		// [CONVENTION] Use convention : in uppercase separated by _ start by K_ for public
+		// [CONVENTION] HeaderDoc for const is compulsory indication
+		/// <summary>
+		/// The K_MY_CONSTANT
+		/// </summary>
+		public const int K_MY_CONSTANT =1 ;
+		// [CONVENTION] Use convention : in uppercase separated by _ start by C_ for private
+		/// <summary>
+		/// The C_MY_CONSTANT is private
+		/// </summary>
+		private const int C_MY_CONSTANT = 1;
 
 		//-------------------------------------------------------------------------------------------------------------
 
@@ -56,18 +97,20 @@ namespace NetWorkedData
 
 		//-------------------------------------------------------------------------------------------------------------
 		// Insert static properties here
-		// Use convention : in CamelCase start by c if private
-		// Use convention : in CamelCase start by k if public
-		// private static TypeClass kSharedInstance;
-		// puoblic static TypeClass cSharedInstance;
-		/// <summary>
-		/// The private shared instance.
-		/// </summary>
-		private static NWDLayoutClass cSharedInstance;
+		// [CONVENTION] all public before private
+		//-------------------------------------------------------------------------------------------------------------
+		// [CONVENTION] Use convention : in CamelCase start by k if public
+		// [CONVENTION] HeaderDoc for properties is compulsory indication
 		/// <summary>
 		/// The public shared instance.
 		/// </summary>
 		public static NWDLayoutClass kSharedInstance;
+		// [CONVENTION] Use convention : in CamelCase start by c if private
+		// [CONVENTION] HeaderDoc for properties is compulsory indication
+		/// <summary>
+		/// The private shared instance.
+		/// </summary>
+		private static NWDLayoutClass cSharedInstance;
 		//-------------------------------------------------------------------------------------------------------------
 
 		#endregion
@@ -78,18 +121,30 @@ namespace NetWorkedData
 
 		//-------------------------------------------------------------------------------------------------------------
 		// Insert instance properties here
-		/// <summary>
-		/// The MEN_BASE.
-		/// </summary>
-		public const string K_MENU_BASE = "NAME";
-		//In UpperCases start by 'K' and plit by '_' if public
-		const string kOverlayLogLimitKey = "myKey";
-		// //In ChamelCase, start by 'k' if private for class
+		// [CONVENTION] all public before private
+		// [CONVENTION] property suffix by User usage
+		// [CONVENTION] xxxxxButton
+		// [CONVENTION] xxxxxLabel
+		// [CONVENTION] xxxxxPanel
+		// [CONVENTION] xxxxxSprite
+		// [CONVENTION] xxxxxGameObject
+		// [CONVENTION] property suffix for specil type 
+		// [CONVENTION] xxxxxList
+		// [CONVENTION] xxxxxDictionary (use name with value/key binding)
+		// [CONVENTION] xxxxxArray
 		//-------------------------------------------------------------------------------------------------------------
-		public bool MyProperties;
-		//In ChamelCase, start by UpperCase ... (no prefix : '_', 'm', 'my', …)
-		public bool AnotherProperty;
-		//In ChamelCase, start by UpperCase ... (no prefix : '_', 'm', 'my', …)
+		// [CONVENTION] Use convention : in CamelCase Start by lowerCase no prefix if public
+		// [CONVENTION] HeaderDoc for properties is compulsory indication
+		/// <summary>
+		/// My properties.
+		/// </summary>
+		public bool activity = true;
+		// [CONVENTION] Use convention : in CamelCase start by lowerCase prefix by '_' if private 
+		// [CONVENTION] HeaderDoc for properties is compulsory indication
+		/// <summary>
+		/// Another property but private.
+		/// </summary>
+		private bool _anotherProperty;
 		//-------------------------------------------------------------------------------------------------------------
 
 		#endregion
@@ -99,7 +154,47 @@ namespace NetWorkedData
 		#region Class methods
 
 		//-------------------------------------------------------------------------------------------------------------
+		// Insert class methods here
+		// [CONVENTION] all public before private
+		//-------------------------------------------------------------------------------------------------------------
+		// [CONVENTION] Use convention : in CamelCase Start by UpperCase for public
+		// [CONVENTION] HeaderDoc for methods is compulsory indication
+		/// <summary>
+		/// My method with parameter.
+		/// </summary>
+		/// <returns><c>true</c>, if method with parameter was myed, <c>false</c> otherwise.</returns>
+		/// <param name="sSentData">If set to <c>true</c> s sent data.</param>
+		public static bool MyMethodWithParam (bool sSentData) // [CONVENTION] sent variable in CamelCase by prefix 's'
+		{
+			bool rReturn = false;
+			bool tTemporaryValue = sSentData; // [CONVENTION] temporary variable in CamelCase by prefix 't'
+
+			// [CONVENTION] no Ternary 
+			if (sSentData == true) { // CONVENTION] use == true or == false, don't use default analyze
+				rReturn = false;
+			} else {
+				rReturn = true;
+			}
+			return rReturn;
+		}
+		//-------------------------------------------------------------------------------------------------------------
 		// Insert static methods here
+		// [CONVENTION] Use convention : in CamelCase Start by UpperCase for private
+		// [CONVENTION] HeaderDoc for methods is compulsory indication
+		/// <summary>
+		/// My method.
+		/// </summary>
+		/// <returns><c>true</c>, if method was my, <c>false</c> otherwise.</returns>
+		private static bool _MyMethod ()
+		{
+			
+			bool rReturn = false; // [CONVENTION] return variable in CamelCase by prefix 'r'
+			// [CONVENTION] add comment for all important part
+			// [CONVENTION] use TODO and FIXME task as soosn as possible
+			// TODO : write this method 
+			// FIXME : fix this method
+			return rReturn;
+		}
 		//-------------------------------------------------------------------------------------------------------------
 
 		#endregion
@@ -110,19 +205,26 @@ namespace NetWorkedData
 
 		//-------------------------------------------------------------------------------------------------------------
 		// Insert instance methods here
+		// [CONVENTION] all public before private
+
+		// [CONVENTION] method suffix by Interface Connexion
+		// [CONVENTION] xxxxxAction if is use as interface connexion action
+
+		//-------------------------------------------------------------------------------------------------------------
+		// [CONVENTION] HeaderDoc for methods is compulsory indication
 		/// <summary>
-		/// My the method. In ChamelCase, start by UpperCase 
+		/// My the method.
 		/// </summary>
-		/// <returns><c>true</c>, if method was myed, <c>false</c> otherwise.</returns>
+		/// <returns><c>true</c>, if method was my, <c>false</c> otherwise.</returns>
 		/// <param name="sSentData">If set to <c>true</c> s sent data.</param>
-		public bool MyMethod (bool sSentData) // s to prefix sent Data
+		public bool MyMethod (bool sSentData) // [CONVENTION] sent variable in CamelCase by prefix 's'
 		{
-			bool rReturnData = false; // r to prefix futur return Data
-			bool tTemporaryData = !sSentData; // t to prefix temporary return Data
-			if (tTemporaryData == false) { // no Ternary 
+			bool rReturnData = false;
+			bool tTemporaryData = !sSentData;
+			if (tTemporaryData == false) {
 				rReturnData = true;
 			}
-
+			// [CONVENTION] use this separator for macro precompile
 			//--------------
 			#if UNITY_EDITOR
 			//--------------
