c26b44cd078925b481cfabcc8865d41a28756b9b
Kortex <jfcontart@idemobi.com>
2018-03-19 16:00:26 +0100

    [ADD] Avatar

---
+++ NWDModules/User/NWDAvatar.cs
@@ -0,0 +1,314 @@
+//=====================================================================================================================
+//
+// ideMobi copyright 2017 
+// All rights reserved by ideMobi
+//
+//=====================================================================================================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using UnityEngine;
+
+using BasicToolBox;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+//=====================================================================================================================
+namespace NetWorkedData
+{
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    /// <summary>
+    /// NWDAvatarConnection can be use in MonBehaviour script to connect GameObject with NWDBasis<Data> in editor.
+    /// Use like :
+    /// public class MyScriptInGame : MonoBehaviour
+    /// { 
+    /// [NWDConnectionAttribut (true, true, true, true)] // optional
+    /// public NWDAvatarConnection MyNetWorkedData;
+    /// }
+    /// </summary>
+    [Serializable]
+    public class NWDAvatarConnection : NWDConnection<NWDAvatar>
+    {
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [NWDClassServerSynchronizeAttribute(true)]
+    [NWDClassTrigrammeAttribute("AVA")]
+    [NWDClassDescriptionAttribute("Avatar composer for user")]
+    [NWDClassMenuNameAttribute("Avatar")]
+    [NWDClassPhpPostCalculateAttribute(" // write your php script here to update $tReference")]
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //[NWDInternalKeyNotEditableAttribute]
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    /// <summary>
+    /// NWD example class. This class is use for (complete description here)
+    /// </summary>
+    public partial class NWDAvatar : NWDBasis<NWDAvatar>
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        #region Class Properties
+        //-------------------------------------------------------------------------------------------------------------
+        // Your static properties
+        //-------------------------------------------------------------------------------------------------------------
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+        #region Instance Properties
+        //-------------------------------------------------------------------------------------------------------------
+        // Your properties
+        //PROPERTIES
+
+        [NWDGroupStart("Account and final render")]
+        public NWDReferenceType<NWDAccount> AccountReference
+        {
+            get; set;
+        }
+        public NWDImagePNGType AvatarRender
+        {
+            get; set;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+        #region Constructors
+        //-------------------------------------------------------------------------------------------------------------
+        public NWDAvatar()
+        {
+            //Debug.Log("NWDAvatar Constructor");
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public NWDAvatar(bool sInsertInNetWorkedData) : base(sInsertInNetWorkedData)
+        {
+            //Debug.Log("NWDAvatar Constructor with sInsertInNetWorkedData : " + sInsertInNetWorkedData.ToString() + "");
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public override void Initialization() // INIT YOUR INSTANCE WITH THIS METHOD
+        {
+           
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+        #region Class methods
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Exampel of implement for class method.
+        /// </summary>
+        public static void MyClassMethod()
+        {
+            // do something with this class
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+        #region Instance methods
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Exampel of implement for instance method.
+        /// </summary>
+        public void MyInstanceMethod()
+        {
+            // do something with this object
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+        #region NetWorkedData addons methods
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just after loaded from database.
+        /// </summary>
+        public override void AddonLoadedMe()
+        {
+            // do something when object was loaded
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before unload from memory.
+        /// </summary>
+        public override void AddonUnloadMe()
+        {
+            // do something when object will be unload
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before insert.
+        /// </summary>
+        public override void AddonInsertMe()
+        {
+            // do something when object will be inserted
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before update.
+        /// </summary>
+        public override void AddonUpdateMe()
+        {
+            // do something when object will be updated
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method when updated.
+        /// </summary>
+        public override void AddonUpdatedMe()
+        {
+            // do something when object finish to be updated
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method when updated me from Web.
+        /// </summary>
+        public override void AddonUpdatedMeFromWeb()
+        {
+            // do something when object finish to be updated from CSV from WebService response
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before dupplicate.
+        /// </summary>
+        public override void AddonDuplicateMe()
+        {
+            // do something when object will be dupplicate
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before enable.
+        /// </summary>
+        public override void AddonEnableMe()
+        {
+            // do something when object will be enabled
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before disable.
+        /// </summary>
+        public override void AddonDisableMe()
+        {
+            // do something when object will be disabled
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before put in trash.
+        /// </summary>
+        public override void AddonTrashMe()
+        {
+            // do something when object will be put in trash
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addon method just before remove from trash.
+        /// </summary>
+        public override void AddonUnTrashMe()
+        {
+            // do something when object will be remove from trash
+            // TODO verif if method is call in good place in good timing
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+        #region Editor
+#if UNITY_EDITOR
+        //-------------------------------------------------------------------------------------------------------------
+        //Addons for Edition
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addons in edition state of object.
+        /// </summary>
+        /// <returns><c>true</c>, if object need to be update, <c>false</c> or not not to be update.</returns>
+        /// <param name="sNeedBeUpdate">If set to <c>true</c> need be update in enter.</param>
+        public override bool AddonEdited(bool sNeedBeUpdate)
+        {
+            if (sNeedBeUpdate == true)
+            {
+                // do something
+            }
+            return sNeedBeUpdate;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addons editor interface.
+        /// </summary>
+        /// <returns>The editor height addon.</returns>
+        /// <param name="sInRect">S in rect.</param>
+        public override float AddonEditor(Rect sInRect)
+        {
+            // Draw the interface addon for editor
+            float tYadd = 0.0f;
+            return tYadd;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Addons editor intreface expected height.
+        /// </summary>
+        /// <returns>The editor expected height.</returns>
+        public override float AddonEditorHeight()
+        {
+            // Height calculate for the interface addon for editor
+            float tYadd = 0.0f;
+            return tYadd;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Adds the width of node draw.
+        /// </summary>
+        /// <returns>The on node draw width.</returns>
+        /// <param name="sDocumentWidth">S document width.</param>
+        public override float AddOnNodeDrawWidth(float sDocumentWidth)
+        {
+            return 250.0f;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Adds the height of node draw.
+        /// </summary>
+        /// <returns>The on node draw height.</returns>
+        public override float AddOnNodeDrawHeight(float sCardWidth)
+        {
+            return 220.0f;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Adds node draw.
+        /// </summary>
+        /// <param name="sRect">S rect.</param>
+        public override void AddOnNodeDraw(Rect sRect, bool sPropertysGroup)
+        {
+            //Rect tSpriteRect = new Rect(sRect.x, sRect.y, 200.0F, 200.0F);
+
+            //if (Face.GetObject() != null)
+            //{
+            //    Face.GetObject().RenderInRect(tSpriteRect, FaceOffsetX, FaceOffsetY, FaceScale, FaceRotation);
+            //}
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Adds color on node.
+        /// </summary>
+        /// <returns>The on node color.</returns>
+        public override Color AddOnNodeColor()
+        {
+            return Color.gray;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+#endif
+        #endregion
+        //-------------------------------------------------------------------------------------------------------------
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+//=====================================================================================================================
