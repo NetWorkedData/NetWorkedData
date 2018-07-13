47958e119c70260ba6be64d86308c72e69246b89
Kortex <jfcontart@idemobi.com>
2018-07-13 19:37:56 +0200

    [STEP] change autolocalized

--- NWDModules/Game/NWDAutolocalized.cs
+++ NWDModules/Game/NWDAutolocalized.cs
@@ -18,15 +18,31 @@ namespace NetWorkedData
     public class NWDAutolocalized : MonoBehaviour
     {
         //-------------------------------------------------------------------------------------------------------------
-        public NWDLocalizationConnection ObjConnection;
+        [Header("Connect the localizable Text")]
+        public NWDLocalizationConnection LocalizationReference;
+        [Header("Optional bind")]
+        public Text TextTarget;
         //-------------------------------------------------------------------------------------------------------------
         void Start()
         {
-            NWDLocalization tObj = ObjConnection.GetObject();
-            if (tObj != null)
+            // if not pluging
+            if (TextTarget == null)
             {
-                string tText = tObj.GetLocalString();
-                GetComponent<Text>().text = tText;
+                // get root text
+                TextTarget = GetComponent<Text>();
+            }
+            // if not find in root object
+            if (TextTarget == null)
+            {
+                // get first children text
+                TextTarget = GetComponentInChildren<Text>();
+            }
+            // set the localizable text
+            NWDLocalization tLocalization = LocalizationReference.GetObject();
+            if (tLocalization != null)
+            {
+                string tTextString = tLocalization.GetLocalString();
+                TextTarget.text = tTextString;
             }
         }
         //-------------------------------------------------------------------------------------------------------------
