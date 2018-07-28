b977da56cd795d4e74d5e68f927b955c2f085789
Kortex <jfcontart@idemobi.com>
2018-07-28 13:22:43 +0200

    [STEP] add autolocalize inspector … Must Have! :-)

--- NWDModules/Game/Localization/NWDAutolocalized.cs
+++ NWDModules/Game/Localization/NWDAutolocalized.cs
@@ -12,6 +12,14 @@ using UnityEngine.UI;
 namespace NetWorkedData
 {
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public enum NWDAutolocalizedTag : int
+    {
+        None,
+        BaseString,
+        MarkedBaseString,
+        KeyInternal,
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     /// <summary>
     /// Autolocalized text with an NWDLocalizationConnection reference selected by popmenu in editor.
     /// </summary>
@@ -23,13 +31,62 @@ namespace NetWorkedData
         /// </summary>
         [Header("Connect the localizable Text")]
         public NWDLocalizationConnection LocalizationReference;
+        public NWDAutolocalizedTag AutoTag = NWDAutolocalizedTag.MarkedBaseString;
         /// <summary>
         /// The text target (optional).
         /// </summary>
         [Header("Optional bind")]
         public Text TextTarget;
         //-------------------------------------------------------------------------------------------------------------
-        void Awake()
+        public void LocalizeEditor()
+        {
+            if (AutoTag != NWDAutolocalizedTag.None)
+            {
+                // if not pluging
+                if (TextTarget == null)
+                {
+                    // get root text
+                    TextTarget = GetComponent<Text>();
+                }
+                // if not find in root object
+                if (TextTarget == null)
+                {
+                    // get first children text
+                    TextTarget = GetComponentInChildren<Text>();
+                }
+                if (TextTarget != null)
+                {
+                    // set the localizable text
+                    NWDLocalization tLocalization = LocalizationReference.GetObject();
+                    if (tLocalization != null)
+                    {
+                        switch (AutoTag)
+                        {
+                            case NWDAutolocalizedTag.BaseString:
+                                {
+                                    string tTextString = tLocalization.TextValue.GetBaseString();
+                                    TextTarget.text = tTextString;
+                                }
+                                break;
+                            case NWDAutolocalizedTag.MarkedBaseString:
+                                {
+                                    string tTextString = tLocalization.TextValue.GetBaseString();
+                                    TextTarget.text = "#" + tTextString + "#";
+                                }
+                                break;
+                            case NWDAutolocalizedTag.KeyInternal:
+                                {
+                                    string tTextString = tLocalization.InternalKeyValue();
+                                    TextTarget.text = tTextString;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void Localize(bool sUseBaseString = false)
         {
             // if not pluging
             if (TextTarget == null)
@@ -43,13 +100,29 @@ namespace NetWorkedData
                 // get first children text
                 TextTarget = GetComponentInChildren<Text>();
             }
-            // set the localizable text
-            NWDLocalization tLocalization = LocalizationReference.GetObject();
-            if (tLocalization != null)
+            if (TextTarget != null)
             {
-                string tTextString = tLocalization.GetLocalString();
-                TextTarget.text = tTextString;
+                // set the localizable text
+                NWDLocalization tLocalization = LocalizationReference.GetObject();
+                if (tLocalization != null)
+                {
+                    if (sUseBaseString == true)
+                    {
+                        string tTextString = tLocalization.TextValue.GetBaseString();
+                        TextTarget.text = tTextString;
+                    }
+                    else
+                    {
+                        string tTextString = tLocalization.GetLocalString();
+                        TextTarget.text = tTextString;
+                    }
+                }
             }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        void Awake()
+        {
+            Localize(false);
         }
         //-------------------------------------------------------------------------------------------------------------
     }
---
+++ NWDModules/Game/Localization/NWDAutolocalizedInspector.cs
@@ -0,0 +1,42 @@
+﻿//=====================================================================================================================
+//
+// ideMobi copyright 2017 
+// All rights reserved by ideMobi
+//
+//=====================================================================================================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+//=====================================================================================================================
+namespace NetWorkedData
+{
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [CustomEditor(typeof(NWDAutolocalized))]
+    public class NWDAutolocalizedInspector : Editor
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        public override void OnInspectorGUI()
+        {
+            NWDAutolocalized tTarget = (NWDAutolocalized)target;
+            DrawDefaultInspector();
+            //if (GUILayout.Button("AutoLocalize copy"))
+            {
+                NWDLocalization tLocalization = tTarget.LocalizationReference.GetObject();
+                if (tLocalization != null)
+                {
+                    tTarget.LocalizeEditor();
+                }
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+//=====================================================================================================================
+#endif
