ccb3f6168c2a7459a14e414c93780fc0c3515d1f
Kortex <jfcontart@idemobi.com>
2018-12-03 13:42:25 +0100

    [STEP] config

--- NWDEditor/NWDAppEnvironment/NWDAppEnvironment_ExtensionWindow.cs
+++ NWDEditor/NWDAppEnvironment/NWDAppEnvironment_ExtensionWindow.cs
@@ -132,10 +132,26 @@ namespace NetWorkedData
 
 
             // DATABASE PARAMS
+            EditorGUILayout.Space();
             EditorGUILayout.HelpBox("Databases", MessageType.None);
-            EditorGUILayout.LabelField("Databases config for all environements", EditorStyles.boldLabel);
+            string tDatabasePathEditor = NWDDataManager.SharedInstance().DatabasePathEditor + "/" + NWDDataManager.SharedInstance().DatabaseNameEditor;
+            string tDatabasePathAccount = NWDDataManager.SharedInstance().DatabasePathAccount + "/" + NWDDataManager.SharedInstance().DatabaseNameAccount;
+
+            EditorGUILayout.LabelField("Databases Editor config for all environements", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Editor path ", tDatabasePathEditor);
+            if (GUILayout.Button("Editor Database File"))
+            {
+               EditorUtility.RevealInFinder(tDatabasePathEditor);
+            }
             /*NWDAppConfiguration.SharedInstance().EditorPass =*/ EditorGUILayout.TextField("Editor Hash Salt", NWDAppConfiguration.SharedInstance().EditorPass);
             EditorGUILayout.TextField("Editor pass ", NWDAppConfiguration.SharedInstance().GetEditorPass());
+
+            EditorGUILayout.LabelField("Databases Accountconfig for all environements (by device)", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Account path ", tDatabasePathAccount);
+            if (GUILayout.Button("Account Database File"))
+            {
+                EditorUtility.RevealInFinder(tDatabasePathAccount);
+            }
            /*NWDAppConfiguration.SharedInstance().AccountHashSalt =*/ EditorGUILayout.TextField("Account Hash Salt", NWDAppConfiguration.SharedInstance().AccountHashSalt);
             EditorGUILayout.TextField("Account pass ", NWDAppConfiguration.SharedInstance().GetAccountPass());
 
@@ -143,6 +159,7 @@ namespace NetWorkedData
 
 
             // WEBSERVICES PARAMS
+            EditorGUILayout.Space();
             EditorGUILayout.HelpBox("Webservices", MessageType.None);
             EditorGUILayout.LabelField("Webservices config for all environements", EditorStyles.boldLabel);
             if (tColum > 1)
@@ -192,7 +209,8 @@ namespace NetWorkedData
             }
 
 
-
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox("Tags", MessageType.None);
 
             EditorGUILayout.LabelField("Tag managment (all environements)", EditorStyles.boldLabel);
             if (tColum > 1)
