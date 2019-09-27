403d932c293424d8a73352b23bd38ee4187f20b2
Kortex <jfcontart@idemobi.com>
2019-09-27 11:33:26 +0200

    [STEP] Change for NetWorkedData_Module_XXX

---
+++ NWDObjects/Items/NWDItemPack.cs
@@ -0,0 +1,64 @@
+﻿//=====================================================================================================================
+//
+//  ideMobi 2019©
+//
+//  Date		2019-4-12 18:49:19
+//  Author		Kortex (Jean-François CONTART) 
+//  Email		jfcontart@idemobi.com
+//  Project 	NetWorkedData for Unity3D
+//
+//  All rights reserved by ideMobi
+//
+//=====================================================================================================================
+
+
+
+using System;
+
+//=====================================================================================================================
+namespace NetWorkedData
+{
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [NWDClassServerSynchronizeAttribute(true)]
+    [NWDClassTrigrammeAttribute("ITP")]
+    [NWDClassDescriptionAttribute("Item Pack descriptions Class")]
+    [NWDClassMenuNameAttribute("Item Pack")]
+    public partial class NWDItemPack : NWDBasis
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        [NWDInspectorGroupStart("Description Item", true, true, true)]
+        public NWDReferenceType<NWDItem> ItemDescription
+        {
+            get; set;
+        }
+        [NWDInspectorGroupEnd]
+
+        [NWDInspectorGroupStart("Items in this Item Pack", true, true, true)]
+        public NWDReferencesQuantityType<NWDItem> Items
+        {
+            get; set;
+        }
+        [NWDInspectorGroupEnd]
+
+        [NWDInspectorGroupStart("Classification", true, true, true)]
+        public NWDReferencesListType<NWDWorld> Worlds
+        {
+            get; set;
+        }
+        public NWDReferencesListType<NWDCategory> Categories
+        {
+            get; set;
+        }
+        public NWDReferencesListType<NWDFamily> Families
+        {
+            get; set;
+        }
+        public NWDReferencesListType<NWDKeyword> Keywords
+        {
+            get; set;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+//=====================================================================================================================
