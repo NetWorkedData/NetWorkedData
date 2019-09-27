9e03fa04c2201f8864666c636bf86d6a5ab5e35c
Kortex <jfcontart@idemobi.com>
2019-09-27 11:39:14 +0200

    [STEP] Change for NetWorkedData_Module_XXX

---
+++ NWDObjects/InAppPurchase/NWDInAppPurchaseWindow.cs
@@ -0,0 +1,47 @@
+﻿//=====================================================================================================================
+//
+//  ideMobi 2019©
+//
+//  Date		2019-4-12 18:49:17
+//  Author		Kortex (Jean-François CONTART) 
+//  Email		jfcontart@idemobi.com
+//  Project 	NetWorkedData for Unity3D
+//
+//  All rights reserved by ideMobi
+//
+//=====================================================================================================================
+
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System;
+
+//=====================================================================================================================
+namespace NetWorkedData
+{
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [NWDTypeWindowParamAttribute("InApp",
+                                 "NWDInAppPurchaseWindow window description",
+        new Type[] {
+            typeof(NWDPack),
+            typeof(NWDItemPack),
+            typeof(NWDItem),
+            typeof(NWDInAppPack),
+        })]
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public class NWDInAppPurchaseWindow : NWDBasisWindow<NWDInAppPurchaseWindow>
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        [MenuItem(NWDConstants.K_MENU_BASE + "Marketplaces/In App Purchase", false, 570)]
+        public static void MenuMethod()
+        {
+            EditorWindow tWindow = GetWindow(typeof(NWDInAppPurchaseWindow));
+            tWindow.Show();
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+//=====================================================================================================================
+#endif
