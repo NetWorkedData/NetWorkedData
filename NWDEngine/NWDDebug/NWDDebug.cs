7ce5fce150218701201992f7520cbe2c4d5535af
Kortex <jfcontart@idemobi.com>
2020-05-09 09:04:16 +0200

    [FIX] build error

--- NWDEngine/NWDDebug/NWDDebug.cs
+++ NWDEngine/NWDDebug/NWDDebug.cs
@@ -1,21 +1,15 @@
 ﻿//=====================================================================================================================
 //
-//  ideMobi 2019©
-//
-//  Date		2019-4-12 18:29:3
-//  Author		Kortex (Jean-François CONTART) 
-//  Email		jfcontart@idemobi.com
-//  Project 	NetWorkedData for Unity3D
-//
+//  ideMobi 2020©
 //  All rights reserved by ideMobi
 //
 //=====================================================================================================================
 
-
-using UnityEditor;
-#if UNITY_EDITOR
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
 #endif
+
 //=====================================================================================================================
 namespace NetWorkedData
 {
