57c8ab0a117b1431d41a90977e6e99953f9a9868
Kortex <jfcontart@idemobi.com>
2020-08-11 16:33:27 +0200

    [LAYOUT] Separate MacroDefiner

---
+++ NWDEditor/NWDEditorWindow/NWDMacroDefineManager/MDEMacroDefine/NWDMacroDefiner.cs
@@ -0,0 +1,221 @@
+﻿//=====================================================================================================================
+//
+//  ideMobi 2020©
+//
+//=====================================================================================================================
+// Define the use of Log and Benchmark only for this file!
+// Add NWD_VERBOSE in scripting define symbols (Edit->Project Settings…->Player->[Choose Plateform]->Other Settings->Scripting Define Symbols)
+#if NWD_VERBOSE
+#if UNITY_EDITOR
+//#define NWD_LOG
+//#define NWD_BENCHMARK
+#elif DEBUG
+//#define NWD_LOG
+//#define NWD_BENCHMARK
+#endif
+#else
+#undef NWD_LOG
+#undef NWD_BENCHMARK
+#endif
+//=====================================================================================================================
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Globalization;
+using UnityEditor;
+//=====================================================================================================================
+namespace NetWorkedData.MacroDefine
+{
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [Serializable]
+    public class NWDMacroDefiner : IComparable
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        public long Value;
+        public string Name;
+        public string Representation;
+        public bool Overridable = true;
+        public string Addon;
+        //-------------------------------------------------------------------------------------------------------------
+        public NWDMacroDefiner()
+        {
+            Value = 0;
+            Name = null;
+            Representation = null;
+            Overridable = true;
+            Addon = null;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public override string ToString()
+        {
+            return Value.ToString(CultureInfo.InvariantCulture);
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public long ToLong()
+        {
+            return Value;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public long GetLong()
+        {
+            return Value;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void SetLong(long sLong)
+        {
+            Value = sLong;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public bool InError = false;
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual float ControlFieldHeight()
+        {
+            return EditorStyles.popup.fixedHeight;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual object ControlField(Rect sPosition, string sEntitled, bool sDisabled, string sTooltips = "")
+        {
+            NWDMacroDefiner tTemporary = new NWDMacroDefiner();
+            tTemporary.Value = Value;
+            //FAKE
+            return tTemporary;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual List<string> StringValuesArray()
+        {
+            List<string> rList = new List<string>();
+            return rList;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual List<string> StringValuesArrayAdd()
+        {
+            List<string> rList = new List<string>();
+            return rList;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual List<string> RepresentationValuesArray()
+        {
+            List<string> rList = new List<string>();
+            return rList;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual bool IsInError()
+        {
+            return InError;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual bool ErrorAnalyze()
+        {
+            InError = false;
+            return InError;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual void Default()
+        {
+            Value = 0;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void Flush()
+        {
+            Value = 0;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual string GetTitle()
+        {
+            return GetType().Name;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual string GetGroup()
+        {
+            return string.Empty;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual int GetOrder()
+        {
+            return 0;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public static bool operator ==(NWDMacroDefiner sA, NWDMacroDefiner sB)
+        {
+            if ((object)sA == null && (object)sB == null)
+            {
+                return true;
+            }
+            else if ((object)sA == null)
+            {
+                return false;
+            }
+            else if ((object)sB == null)
+            {
+                return false;
+            }
+            else
+            {
+                var typeMatches = sA.GetType().Equals(sB.GetType());
+                if (typeMatches)
+                {
+                    return sA.Value.Equals(sB.Value);
+                }
+            }
+            return false;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public static bool operator !=(NWDMacroDefiner sA, NWDMacroDefiner sB)
+        {
+            if ((object)sA == null && (object)sB == null)
+            {
+                return false;
+            }
+            else if ((object)sA == null)
+            {
+                return true;
+            }
+            else if ((object)sB == null)
+            {
+                return true;
+            }
+            else
+            {
+                var typeMatches = sA.GetType().Equals(sB.GetType());
+                if (typeMatches)
+                {
+                    return !sA.Value.Equals(sB.Value);
+                }
+            }
+            return false;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as NWDMacroDefiner;
+
+            if (otherValue == null)
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Value.Equals(otherValue.Value);
+
+            return typeMatches && valueMatches;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public int CompareTo(object sOther) => Value.CompareTo(((NWDMacroDefiner)sOther).Value);
+        //-------------------------------------------------------------------------------------------------------------
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public class NWDMacroBoolDefiner : NWDMacroDefiner
+    {
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public class NWDMacroEnumDefiner : NWDMacroDefiner
+    {
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+//=====================================================================================================================
+#endif
