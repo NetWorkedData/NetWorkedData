777fe2197ff9eddd81a9f8107d35a175d86ef595
jfcontart <jfcontart@idemobi.com>
2020-10-17 09:37:13 +0200

    [ISSUE] 146
    https://gitlab.hephaiscode.com/idemobi-games/NetWorkedData/-/issues/146

--- NWDModels/App/NWDVersion/NWDVersion_Editor.cs
+++ NWDModels/App/NWDVersion/NWDVersion_Editor.cs
@@ -98,131 +98,42 @@ namespace NetWorkedData
         //-------------------------------------------------------------------------------------------------------------
         public override void AddonEditor(Rect sRect)
         {
-            // force update 
-            //NWDVersion.UpdateVersionBundle();
-            // show editor add-on
-            //float tWidth = sInRect.width - NWDGUI.kFieldMarge * 2;
-            //float tX = sInRect.position.x + NWDGUI.kFieldMarge;
-            //float tY = sInRect.position.y + NWDGUI.kFieldMarge;
-            // darw information about actual bundle 
-            Rect sInRect = NWDGUI.MargeLeftRight(sRect);
-            // Draw line 
-            sInRect.y += NWDGUI.Separator(sInRect).height;
-
+            // force update
             EditorGUI.BeginDisabledGroup(true);
-
-            sInRect.height = NWDGUI.kBoldLabelStyle.fixedHeight;
-            GUI.Label(sInRect, "Environement selected to build", NWDGUI.kBoldLabelStyle);
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-
-            sInRect.height = NWDGUI.kLabelStyle.fixedHeight;
-            EditorGUI.LabelField(sInRect, "Environment", NWDAppConfiguration.SharedInstance().SelectedEnvironment().Environment, NWDGUI.kLabelStyle);
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-
-            EditorGUI.LabelField(sInRect, "Version", PlayerSettings.bundleVersion, NWDGUI.kLabelStyle);
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-
+            GUILayout.Label("Environement selected to build", NWDGUI.kBoldLabelStyle);
+            EditorGUILayout.LabelField("Environment", NWDAppConfiguration.SharedInstance().SelectedEnvironment().Environment, NWDGUI.kLabelStyle);
+            EditorGUILayout.LabelField("Version", PlayerSettings.bundleVersion, NWDGUI.kLabelStyle);
             EditorGUI.EndDisabledGroup();
-
-            // Draw line 
-            sInRect.y += NWDGUI.Separator(sInRect).height;
-
-            sInRect.height = NWDGUI.kMiniButtonStyle.fixedHeight;
-            if (GUI.Button(sInRect, "Recommendation by SMS", NWDGUI.kMiniButtonStyle))
+            NWDGUILayout.Separator();
+            if (GUILayout.Button("Recommendation by SMS", NWDGUI.kMiniButtonStyle))
             {
                 RecommendationBy(NWDRecommendationType.SMS);
             }
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-            if (GUI.Button(sInRect, "Recommendation by Email", NWDGUI.kMiniButtonStyle))
+            if (GUILayout.Button("Recommendation by Email", NWDGUI.kMiniButtonStyle))
             {
                 RecommendationBy(NWDRecommendationType.Email);
             }
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-            if (GUI.Button(sInRect, "Recommendation by Email HTML", NWDGUI.kMiniButtonStyle))
+            if (GUILayout.Button("Recommendation by Email HTML", NWDGUI.kMiniButtonStyle))
             {
                 RecommendationBy(NWDRecommendationType.EmailHTML);
             }
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-
-            // Draw line 
-            sInRect.y += NWDGUI.Separator(sInRect).height;
-
-            // Draw QRCode texture
-            //if (QRCodeTexture == null)
-            //{
-            //    QRCodeTexture = FlashMyApp(false, 256);
-            //}
-            //EditorGUI.DrawPreviewTexture(new Rect(sInRect.x, sInRect.y, NWDGUI.kPrefabSize * 2, NWDGUI.kPrefabSize * 2), QRCodeTexture);
-            //sInRect.y += NWDGUI.kPrefabSize * 2 + NWDGUI.kFieldMarge;
-
-            // Draw line 
-            sInRect.y += NWDGUI.Separator(sInRect).height;
-
-
-            sInRect.height = NWDGUI.kLabelStyle.fixedHeight;
-            EditorGUI.TextField(sInRect, "URL My App", URLMyApp(false), NWDGUI.kLabelStyle);
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-            sInRect.height = NWDGUI.kMiniButtonStyle.fixedHeight;
-            if (GUI.Button(sInRect, "URL My App without redirection", NWDGUI.kMiniButtonStyle))
+            NWDGUILayout.Separator();
+            EditorGUILayout.TextField("URL My App", URLMyApp(false), NWDGUI.kLabelStyle);
+            if (GUILayout.Button("URL My App without redirection", NWDGUI.kMiniButtonStyle))
             {
                 Application.OpenURL(URLMyApp(false));
             }
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-
-            sInRect.height = NWDGUI.kLabelStyle.fixedHeight;
-            EditorGUI.TextField(sInRect, "URL My App", URLMyApp(true), NWDGUI.kLabelStyle);
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-            sInRect.height = NWDGUI.kMiniButtonStyle.fixedHeight;
-            if (GUI.Button(sInRect, "URL My App with redirection", NWDGUI.kMiniButtonStyle))
+            EditorGUILayout.TextField("URL My App", URLMyApp(true), NWDGUI.kLabelStyle);
+            if (GUILayout.Button("URL My App with redirection", NWDGUI.kMiniButtonStyle))
             {
                 Application.OpenURL(URLMyApp(true));
             }
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
-            // Draw line 
-            sInRect.y += NWDGUI.Separator(NWDGUI.MargeLeftRight(sInRect)).height;
-            // Draw button choose env
-            if (GUI.Button(sInRect, "Environment chooser", NWDGUI.kMiniButtonStyle))
+            NWDGUILayout.Separator();
+            if (GUILayout.Button("Environment chooser", NWDGUI.kMiniButtonStyle))
             {
-                //NWDEditorMenu.EnvironementChooserShow();
                 NWDEditorMenu.ChooserWindow();
             }
-            sInRect.y += sInRect.height + NWDGUI.kFieldMarge;
         }
-        //-------------------------------------------------------------------------------------------------------------
-        public override float AddonEditorHeight(float sWidth)
-        {
-            // Height calculate for the interface addon for editor
-            GUIStyle tTextFieldStyle = new GUIStyle(EditorStyles.textField);
-            tTextFieldStyle.fixedHeight = tTextFieldStyle.CalcHeight(new GUIContent(NWEConstants.K_A), 100);
-            GUIStyle tMiniButtonStyle = new GUIStyle(EditorStyles.miniButton);
-            tMiniButtonStyle.fixedHeight = tMiniButtonStyle.CalcHeight(new GUIContent(NWEConstants.K_A), 100);
-
-            float tYadd = 0.0f;
-
-            tYadd += NWDGUI.kFieldMarge;
-
-            tYadd += tTextFieldStyle.fixedHeight + NWDGUI.kFieldMarge;
-
-            tYadd += tTextFieldStyle.fixedHeight + NWDGUI.kFieldMarge;
-
-            tYadd += tTextFieldStyle.fixedHeight + NWDGUI.kFieldMarge;
-
-            tYadd += tTextFieldStyle.fixedHeight + NWDGUI.kFieldMarge;
-            tYadd += tTextFieldStyle.fixedHeight + NWDGUI.kFieldMarge;
-
-            tYadd += NWDGUI.kFieldMarge;
-
-            tYadd += tMiniButtonStyle.fixedHeight + NWDGUI.kFieldMarge;
-            tYadd += tMiniButtonStyle.fixedHeight + NWDGUI.kFieldMarge;
-            tYadd += tMiniButtonStyle.fixedHeight + NWDGUI.kFieldMarge;
-
-            tYadd += NWDGUI.kPrefabSize * 2 + NWDGUI.kFieldMarge;
-
-            tYadd += NWDGUI.kFieldMarge;
-
-            return tYadd;
-        }
-
         //-------------------------------------------------------------------------------------------------------------
         private static string DefaultVersionReference()
         {
