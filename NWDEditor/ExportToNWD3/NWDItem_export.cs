19fda0b7683af8ed61a49627cc181fbaaeb19034
Jérôme DEMYTTENAERE <jerome.demyttenaere@gmail.com>
2023-09-06 16:55:02 +0200

    [STEP] json export NWDItem to NWD3

--- NWDEditor/ExportToNWD3/NWDItem_export.cs
+++ NWDEditor/ExportToNWD3/NWDItem_export.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
 
 namespace NetWorkedData
 {
@@ -7,38 +9,77 @@ namespace NetWorkedData
     {
         public override List<NWDExportObject> ExportNWD3(ulong sProjectHub, ulong sProjectId)
         {
-            List<NWDExportObject> rReturn = new List<NWDExportObject>();
             // Create object for Language!
+            var tExport = new {
+                Name = NWDExportObject.ProcessNewLocalizedString(sProjectHub, sProjectId, Name),
+                PluralName = NWDExportObject.ProcessNewLocalizedString(sProjectHub, sProjectId, PluralName),
+                SubName = NWDExportObject.ProcessNewLocalizedString(sProjectHub, sProjectId, SubName),
+                Description = NWDExportObject.ProcessNewLocalizedString(sProjectHub, sProjectId, Description),
+                FirstAcquisitionNotification = 0,
+                AddItemNotification = 0,
+                RemoveItemNotification = 0,
+                NoMoreItemNotification = 0,
+                Rarity = 0,
+                HiddenInGame = 0,
+                Uncountable = 0,
+                Usable = 1,
+                DelayBeforeUse = 0,
+                DurationOfUse = 0,
+                DelayBeforeReUse = 0,
+                ItemExtensionQuantity = "",
+                ParameterList = "",
+                PrimarySprite = "**Assets/Babaoo_Graph/Resources/Pictograms/Simple Inhibition.png**",
+                SecondarySprite = "",
+                TertiarySprite = "",
+                PrimaryTexture = "",
+                SecondaryTexture = "",
+                TertiaryTexture = "",
+                PrimaryColor = "2753FAFF",
+                SecondaryColor = "000000FF",
+                TertiaryColor = "000000FF",
+                PrimaryPrefab = "",
+                SecondaryPrefab = "",
+                TertiaryPrefab = "",
+                EffectPrefab = "",
+                JSON = "",
+                KeysValues = "",
+                ItemGroupList = "",
+                RangeAccess = 0,
+                ID = 9,
+                Reference = "62860253-481690",
+                CheckList = 0,
+                WebModel = 10,
+                InternalKey = "Pictogram - Simple Inhibition",
+                InternalDescription = "",
+                Preview = "",
+                AC = 1,
+                DC = 1651497053,
+                DM = 1673434260,
+                DD = 0,
+                XX = 0,
+                Integrity = "",
+                DS = 1673434260,
+                DevSync = 1692018323,
+                PreprodSync = 0,
+                ProdSync = 0,
+                Tag = 0,
+                ServerHash = "",
+                ServerLog = "",
+                InError = 0,
+                CraftBookAttachment = "",
+                CategoryList = "",
+                FamilyList = "",
+                KeywordList = ""
+            };
 
-            NWDExportObject tName = new NWDExportObject(sProjectHub, sProjectId, Name);
-            rReturn.Add(tName);
+            //string tJsonUnity = JsonUtility.ToJson(tExport);
+            string tJson = JsonConvert.SerializeObject(tExport);
+
             // Create object for Asset!
-            
             // no asset here
-            
             // Create root object
-            string tJson = "{"+
-                             "\""+nameof(Reference)+"\":"+NWDToolbox.NumericCleaner(Reference)+", " +
-                             //-------------------------
-                             //-------------------------
-                             //-------------------------
-                             
-                             
-                             //Insert Here your Object
-                
-                             "\"Rarity\":"+Rarity.ToString("0.00000")+", " +
-                             
-                             // if you create object for langague or asset, use it 
-                             
-                            "\"Name\":"+tName.Link()+", " +
-                             
-                             //-------------------------
-                             //-------------------------
-                             //-------------------------
-                             //-------------------------
-                             "\""+nameof(AC)+"\":1" +
-                             "}";
 
+            List<NWDExportObject> rReturn = new List<NWDExportObject>();
             NWDExportObject tObject = new NWDExportObject(sProjectHub, sProjectId, Reference, InternalKey, InternalDescription, tJson, nameof(NWDItem), false);
             rReturn.Add(tObject);
             return rReturn;
